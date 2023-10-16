3cb68ee502fe48cda5ba4298b68a9ad861d32ab3
Srinivasa R <srinivasa.r0503@gmail.com>
2023-10-16 16:16:22 +0530

    [Utility] Added read8 and write8

--- include/RAM.hpp
+++ include/RAM.hpp
@@ -10,6 +10,8 @@ public:
     RAM(uint32_t size);
     uint32_t read32_cpu(uint32_t offset);
     void write32_cpu(uint32_t offset, uint32_t data);
+    uint8_t read8_cpu(uint32_t offset);
+    void write8_cpu(uint32_t offset, uint8_t data);
 private:
     std::vector<uint8_t> data;
 };
--- src/RAM.cpp
+++ src/RAM.cpp
@@ -41,3 +41,25 @@ void RAM::write32_cpu(uint32_t offset, uint32_t data)
     }
     *(uint32_t*)&this->data[offset] = data;
 }
+
+uint8_t RAM::read8_cpu(uint32_t offset)
+{
+    if(offset >= data.size())
+    {
+        std::stringstream ss;
+        ss << "Size exceeded for read8_cpu (RAM): 0x" << std::hex << offset;
+        throw std::runtime_error(ss.str());
+    }
+    return data[offset];
+}
+
+void RAM::write8_cpu(uint32_t offset, uint8_t data)
+{
+    if(offset >= this->data.size())
+    {
+        std::stringstream ss;
+        ss << "Size exceeded for write8_cpu (RAM): 0x" << std::hex << offset;
+        throw std::runtime_error(ss.str());
+    }
+    this->data[offset] = data;
+}
